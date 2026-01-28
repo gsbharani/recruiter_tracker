b419a8f5bf0ff7eae69c4a408db3bea3d0893cc1
gsbharani <gsbharani45@gmail.com>
2026-01-28 10:18:39 +0530

    Update matcher.py

--- matcher.py
+++ matcher.py
@@ -8,3 +8,13 @@ def semantic_score(jd_text: str, resume_text: str) -> float:
 
     similarity = util.cos_sim(jd_emb, resume_emb).item()
     return round(similarity * 100, 2)
+
+def skill_score(resume_text, skills):
+    resume_text = resume_text.lower()
+    matched = sum(1 for skill in skills if skill in resume_text)
+
+    if not skills:
+        return 0
+
+    return (matched / len(skills)) * 100
+
